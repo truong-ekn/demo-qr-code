a72b157ec6797ea34d526d7f813e672f2b956bd4
minhtruong.trieu <minhtruong.trieu@ekino.com>
2024-01-30 17:16:26 +0700

    place data to screen

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -1,33 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
 const QRScanner = () => {
-  function onScanSuccess(decodedText: any, decodedResult: any) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-  }
+  const [data, setData] = useState<string[]>([]);
 
-  function onScanFailure(error: string) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
-  }
   useEffect(() => {
     const html5QRCodeScanner = new Html5QrcodeScanner(
       qrcodeRegionId,
       { fps: 10, qrbox: { width: 250, height: 250 } },
       true
     );
-    html5QRCodeScanner.render(onScanSuccess, onScanFailure);
+
+    html5QRCodeScanner.render(
+      (decodedText, result) => {
+        console.log(`Code matched = ${decodedText}`, result);
+        setData((prev) => [...prev, decodedText]);
+      },
+      (error) => {
+        console.warn(`Code scan error = ${error}`);
+      }
+    );
     return () => {
       html5QRCodeScanner.clear().catch((error) => {
         console.error("Failed to clear html5QrcodeScanner. ", error);
       });
     };
   }, []);
-  return <div id={qrcodeRegionId}></div>;
+  return (
+    <div>
+      <div id={qrcodeRegionId}></div>
+      <ul>
+        {data.map((item, idx) => (
+          <li key={idx}>
+            <a href={item}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default QRScanner;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
